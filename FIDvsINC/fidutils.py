e05db99fc8ac7b7995157f71d0143ca5b0714902
Seungwook Han <sh3264@coumbia.edu>
2020-05-29 19:07:31 -0400

    metric calculation with norm

--- FIDvsINC/fidutils.py
+++ FIDvsINC/fidutils.py
@@ -57,6 +57,31 @@ def get_softmax(sess, pool3):
     return softmax
 #===============================================================================
 
+# Normalize image
+def normalize_img(data, range=None, scale_each=False):
+    data = data.clone()  # avoid modifying tensor in-place
+    if range is not None:
+        assert isinstance(range, tuple), \
+            "range has to be a tuple (min, max) if specified. min and max are numbers"
+
+    def norm_ip(img, min, max):
+        img.clamp_(min=min, max=max)
+        img.add_(-min).div_(max - min + 1e-5)
+
+    def norm_range(t, range):
+        if range is not None:
+            norm_ip(t, range[0], range[1])
+        else:
+            norm_ip(t, float(t.min()), float(t.max()))
+
+    if scale_each is True:
+        for t in data:  # loop over mini-batch dimension
+            norm_range(t, range)
+    else:
+        norm_range(data, range)
+
+    return data
+
 
 #
 # simple data container with image transformations
